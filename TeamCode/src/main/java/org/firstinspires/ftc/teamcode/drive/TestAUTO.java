08daca23d596c2dc00a73e7ba42d0777b71bdbda
richpant <47926579+richpant@users.noreply.github.com>
2021-12-15 11:22:24 -0800

    Update the Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TestAUTO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TestAUTO.java
@@ -1,37 +1,80 @@
 package org.firstinspires.ftc.teamcode.drive;
 
+import static com.qualcomm.robotcore.hardware.DcMotor.*;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
-@Autonomous(name = "TestAUTO")
+@Autonomous(name = "RedSideAUTOTestAUTO")
 public class TestAUTO extends LinearOpMode {
-
+    private DcMotor liftMotor;
+    private DcMotor twist;
+    private DcMotor carry;
+    private Servo bucket;
+    RevBlinkinLedDriver coolLights;
 
     @Override
     public void runOpMode() {
-
+        twist = hardwareMap.get(DcMotor.class, "twist");
+        carry = hardwareMap.get(DcMotor.class, "carry");
+        liftMotor = hardwareMap.get(DcMotor.class, "liftMotor");
+        bucket = hardwareMap.get(Servo.class, "bucket");
+        coolLights = hardwareMap.get(RevBlinkinLedDriver.class, "coolLights");
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-
+        //liftMotor.setMode(RunMode.STOP_AND_RESET_ENCODER);
         Pose2d startPose = new Pose2d(-23.6, -69.6, Math.PI / 2);
         drive.setPoseEstimate(startPose);
-
+        coolLights.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_STROBE);
+        //liftMotor.getZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
         TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
-                .forward(20)
-                .turn(Math.toRadians(180))
-//                .addTemporalMarker(10, () -> {
-//                    // This marker runs two seconds into the trajectory
-//
-//                    // Run your action in here!
-//                })
+                .back(34.2)
+                .addTemporalMarker(() -> lift(1700))
+                .addTemporalMarker(() -> bucket.setPosition(0.7))
+                .waitSeconds(3)
+                .addTemporalMarker(() -> bucket.setPosition(-0.5))
+                .addTemporalMarker(() -> lift(-1700))
+                .strafeRight(79)
+                .forward(29)
+                .addTemporalMarker(()-> liftMotor.setPower(-0.4))
+                .waitSeconds(2)
+                .addTemporalMarker(()-> liftMotor.setPower(0))
+                .back(20)
                 .build();
         waitForStart();
 
         if (!isStopRequested())
             drive.followTrajectorySequence(trajSeq);
     }
-}
+    public void lift(int encod)
+    {
+
+            carry.setTargetPosition(encod);
+
+            carry.setMode(RunMode.RUN_TO_POSITION);
+
+            carry.setPower(1);
+            while(carry.isBusy())
+            {
+                sleep(50);
+            }
+    }
+
+    public void duck(int encod)
+    {
+
+        liftMotor.setTargetPosition(encod);
+
+        liftMotor.setMode(RunMode.RUN_TO_POSITION);
+
+        liftMotor.setPower(.7);
+
+        }
+    }
